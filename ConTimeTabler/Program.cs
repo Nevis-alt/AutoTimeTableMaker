80fb978395f7808659d4d14853653d066500f33a
Nevis-alt <doublebackup0713@gmail.com>
2025-09-09 06:39:08 +0000

    Include

---
+++ ConTimeTabler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+
+class Program
+{
+    static void Main()
+    {
+        var workbook = new XLWorkbook("Data/DataBase.xlsx");
+        var worksheet = workbook.Worksheet(1);
+
+        var name = worksheet.Cell(2, 2).GetString();
+        var age = worksheet.Cell(2, 3).GetString();
+
+        Console.WriteLine($"이름: {name}, 나이: {age}");
+    }
+}
